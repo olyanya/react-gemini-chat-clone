fa31064187ecfc49caf3b3aca8109a832fa49c4a
olyanya <you@example.com>
2024-05-07 15:34:48 -0400

    cleanup: removed unnecessary files & added a new ones

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./components/Main/Main";
+import Sidebar from "./components/Sidebar/Sidebar";
+
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+};
+
+export default App;
