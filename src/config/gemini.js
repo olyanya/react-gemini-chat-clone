85f749c952c3974ecbaf49495346e748b5c1a26b
olyanya <you@example.com>
2024-05-07 15:38:35 -0400

    feat: added gemini API

---
+++ src/config/gemini.js
@@ -0,0 +1,55 @@
+// node --version # Should be >= 18
+// npm install @google/generative-ai
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const MODEL_NAME = "gemini-1.5-pro-latest";
+const VITE_REACT_APP_API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
+
+async function runChat(prompt) {
+  const genAI = new GoogleGenerativeAI(VITE_REACT_APP_API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  const generationConfig = {
+    temperature: 1,
+    topK: 0,
+    topP: 0.95,
+    maxOutputTokens: 8192,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [],
+  });
+
+  const result = await chat.sendMessage(prompt);
+  const response = result.response;
+  console.log(response.text());
+  return response.text();
+}
+
+export default runChat;
